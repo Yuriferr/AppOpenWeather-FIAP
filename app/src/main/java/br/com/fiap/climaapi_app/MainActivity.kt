0b87bdcdf2c9fdd7e69cf4fa798ab011104b73a3
Yuriferr <yurifernandespreto@gmail.com>
2024-03-11 15:06:38 -0300

    Segundo commit

--- app/src/main/java/br/com/fiap/climaapi_app/MainActivity.kt
+++ app/src/main/java/br/com/fiap/climaapi_app/MainActivity.kt
@@ -1,41 +1,54 @@
 package br.com.fiap.climaapi_app
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Address
+import android.location.Geocoder
+import android.location.Location
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
-import androidx.compose.material3.Card
-import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.input.KeyboardCapitalization
-import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat
 import br.com.fiap.climaapi_app.model.OpenWeather
 import br.com.fiap.climaapi_app.service.RetrofitFactory
 import br.com.fiap.climaapi_app.ui.theme.ClimaAPIAppTheme
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import java.util.Locale
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -47,74 +60,158 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    ScreenWeather()
+                    Screen()
                 }
             }
         }
     }
 }
 
-//18cad22e69252aa4dd95daa8da20c3f3
-
-
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun ScreenWeather() {
+fun Screen() {
+    var dataState by remember { mutableStateOf<OpenWeather?>(null) }
+    var isLoading by remember { mutableStateOf(false) }
+    var errorMessage by remember { mutableStateOf<String?>(null) }
 
-    var cityState by remember {
-        mutableStateOf("")
-    }
+    val context = LocalContext.current
+    val fusedLocationClient: FusedLocationProviderClient =
+        LocationServices.getFusedLocationProviderClient(context)
 
-    var dataState by remember {
-        mutableStateOf<OpenWeather?>(null)
-    }
-
-    Column(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(20.dp)
-    ) {
-        Row(verticalAlignment = Alignment.CenterVertically) {
-            OutlinedTextField(
-                value = cityState,
-                onValueChange = {
-                    cityState = it
-                },
-                modifier = Modifier.weight(2f),
-                label = {
-                    Text(text = "Nome da cidade")
-                },
-                keyboardOptions = KeyboardOptions(
-                    keyboardType = KeyboardType.Text,
-                    capitalization = KeyboardCapitalization.Words
-                )
-            )
-            IconButton(onClick = {
-                val call = RetrofitFactory().getOpenWeatherService().getWeatherByCity(cityState)
-                call.enqueue(object : Callback<OpenWeather> {
-                    override fun onResponse(
-                        call: Call<OpenWeather>,
-                        response: Response<OpenWeather>
-                    ) {
-                        if (response.isSuccessful) {
-                            dataState = response.body()
-                            Log.d("FIAP", "Response: $dataState")
-                        } else {
-                            Log.e("FIAP", "Response not successful: ${response.code()} ${response.message()}")
-                        }
-                    }
-
-                    override fun onFailure(call: Call<OpenWeather>, t: Throwable) {
-                        Log.i("FIAP", "onResponse: ${t.message}")
-                    }
-
-                })
-            }
+    fun fetchData(city: String) {
+        isLoading = true
+        val call = RetrofitFactory().getOpenWeatherService().getWeatherByCity(city)
+        call.enqueue(object : Callback<OpenWeather> {
+            override fun onResponse(
+                call: Call<OpenWeather>,
+                response: Response<OpenWeather>
             ) {
-                Icon(imageVector = Icons.Default.Search, contentDescription = "")
+                if (response.isSuccessful) {
+                    dataState = response.body()
+                    errorMessage = null
+                } else {
+                    errorMessage =
+                        "Erro na resposta da API: ${response.code()} ${response.message()}"
+                }
+                isLoading = false
+            }
+
+            override fun onFailure(call: Call<OpenWeather>, t: Throwable) {
+                errorMessage = "Falha na chamada da API: ${t.message}"
+                isLoading = false
+            }
+        })
+    }
+
+    fun getLocationAndFetchWeather() {
+        isLoading = true
+        fusedLocationClient.lastLocation
+            .addOnSuccessListener { location: Location? ->
+                location?.let {
+                    val geocoder = Geocoder(context, Locale.getDefault())
+                    val addresses: List<Address> =
+                        geocoder.getFromLocation(
+                            location.latitude,
+                            location.longitude,
+                            1
+                        ) as List<Address>
+                    val cityName = addresses[0].locality
+                    fetchData(cityName)
+                } ?: run {
+                    isLoading = false
+                    errorMessage = "Localização não disponível"
+                }
+            }
+            .addOnFailureListener { e ->
+                isLoading = false
+                errorMessage = "Erro ao obter localização: ${e.message}"
+            }
+    }
+
+    val requestPermissionLauncher = rememberLauncherForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted: Boolean ->
+        if (isGranted) {
+            getLocationAndFetchWeather()
+        } else {
+            errorMessage = "Permissão de localização negada"
+        }
+    }
+
+    fun checkLocationPermission() {
+        when {
+            ContextCompat.checkSelfPermission(
+                context,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED -> {
+                getLocationAndFetchWeather()
+            }
+
+            else -> {
+                requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
             }
         }
-        dataState?.weather?.firstOrNull()?.let { Text(text = it.main) }
+    }
+
+    LaunchedEffect(Unit) {
+        checkLocationPermission()
+    }
+
+    if (isLoading) {
+        // Tela de carregamento
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            CircularProgressIndicator()
+        }
+    } else {
+        // Tela mostrando as informações da API
+        if (errorMessage != null) {
+            // Exibir mensagem de erro, se houver
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(text = errorMessage!!)
+            }
+        } else {
+            // Exibir dados do clima
+            dataState?.let { ScreenWeather(weather = it) }
+        }
+    }
+}
+
+
+@Composable
+fun ScreenWeather(weather: OpenWeather) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(30.dp)
+            .background(Color.Magenta)
+    ) {
+        Column(modifier = Modifier.fillMaxWidth()) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceEvenly
+            ) {
+                IconButton(
+                    onClick = { /*TODO*/ }, modifier = Modifier
+                        .background(Color.White, CircleShape)
+                        .size(30.dp), enabled = false
+                ) {
+                    Icon(Icons.Filled.Search, contentDescription = "invisible")
+                }
+                weather.weather.firstOrNull()?.description?.let { Text(text = it) }
+                IconButton(
+                    onClick = { /*TODO*/ }, modifier = Modifier
+                        .background(Color.White, CircleShape)
+                        .size(30.dp)
+                ) {
+                    Icon(Icons.Filled.Search, contentDescription = "pesquisa")
+                }
+            }
+        }
     }
 }
 
--- app/src/main/java/br/com/fiap/climaapi_app/model/OpenWeather.kt
+++ app/src/main/java/br/com/fiap/climaapi_app/model/OpenWeather.kt
@@ -1,65 +1,22 @@
 package br.com.fiap.climaapi_app.model
 
-import com.google.gson.annotations.SerializedName
-
 data class OpenWeather(
-    @SerializedName("coord") val coordinates: Coordinates,
     val weather: List<Weather>,
-    val base: String,
     val main: Main,
-    val visibility: Int,
     val wind: Wind,
-    val rain: Rain?,
-    val clouds: Clouds,
-    val timestamp: Long,
-    val sys: Sys,
-    val timezone: Int,
-    val id: Int,
-    @SerializedName("name") val cityName: String,
-    val cod: Int
-)
-
-data class Coordinates(
-    @SerializedName("lon") val longitude: Double,
-    @SerializedName("lat") val latitude: Double
 )
 
 data class Weather(
-    val id: Int,
     val main: String,
     val description: String,
-    val icon: String
 )
 
 data class Main(
-    @SerializedName("temp") val temperature: Double,
-    @SerializedName("feels_like") val feelsLike: Double,
-    @SerializedName("temp_min") val minTemperature: Double,
-    @SerializedName("temp_max") val maxTemperature: Double,
-    val pressure: Int,
     val humidity: Int,
-    @SerializedName("sea_level") val seaLevel: Int,
-    @SerializedName("grnd_level") val groundLevel: Int
 )
 
 data class Wind(
     val speed: Double,
     val deg: Int,
     val gust: Double
-)
-
-data class Rain(
-    @SerializedName("1h") val oneHour: Double
-)
-
-data class Clouds(
-    val all: Int
-)
-
-data class Sys(
-    val type: Int,
-    val id: Int,
-    val country: String,
-    val sunrise: Long,
-    val sunset: Long
 )
